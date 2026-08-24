6d563cfbe411f540ae68aa01373e91b7dbcf8020
Alyx <90263311+exenal12@users.noreply.github.com>
2026-08-24 14:50:34 -0500

    improve animation script

--- scripts/background.js
+++ scripts/background.js
@@ -1,11 +1,11 @@
-const STAR_COUNT = 200;
+const STAR_COUNT = 500;
 const MIN_DEPTH = 1;
 const MAX_DEPTH = 1000;
-const MAX_RADIUS = 3;
+const MAX_RADIUS = 4;
 const STAR_RADIUS = (zCoord) =>
-  Math.max(0.5, (1 - zCoord / MAX_DEPTH) * MAX_RADIUS);
+  Math.max(0, (1 - zCoord / MAX_DEPTH) * MAX_RADIUS);
 const STAR_SPEED = 1;
-const FOV = 200;
+const FOV = 400;
 const CENTER = { X: window.innerWidth / 2, Y: window.innerHeight / 2 };
 
 const can = document.getElementById("canvas");
@@ -26,21 +26,37 @@ function randomStar(canvas) {
   };
 }
 
+/*
+animation steps
+1. clear screen
+2. new path
+3. calculate new z
+4. for each star, calculate radius and projection
+5. check if too close/off screen, delete/recycle if needed
+6. draw star
+*/
 function frame() {
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
   ctx.beginPath();
   for (const star of stars) {
-    // reset star if it gets too close
-    if (star.z >= MIN_DEPTH) star.z -= STAR_SPEED;
-    else {
-      star.x = (Math.random() - 0.5) * window.innerWidth;
-      star.y = (Math.random() - 0.5) * window.innerHeight;
-      star.z = MAX_DEPTH;
-    }
+    star.z -= STAR_SPEED;
     const x = CENTER.X + (star.x / star.z) * FOV;
     const y = CENTER.Y + (star.y / star.z) * FOV;
     const radius = STAR_RADIUS(star.z);
 
+    const margin = 50;
+    const offScreen =
+      x < -margin ||
+      x > window.innerWidth + margin ||
+      y < -margin ||
+      y > window.innerHeight + margin;
+
+    if (star.z < MIN_DEPTH || offScreen) {
+      star.x = (Math.random() - 0.5) * window.innerWidth;
+      star.y = (Math.random() - 0.5) * window.innerHeight;
+      star.z = MAX_DEPTH;
+      continue;
+    }
     ctx.moveTo(x + radius, y);
     ctx.arc(x, y, radius, 0, Math.PI * 2);
   }
@@ -48,15 +64,6 @@ function frame() {
   ctx.fill();
   window.requestAnimationFrame(frame);
 }
-
-/*
-animation steps
-1. change star z coordinate
-2. compute screen position from that state
-3. clear canvas
-4. draw again with new star state
-5. requestAnimationFrame
-*/
 
 ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
 const stars = Array.from({ length: STAR_COUNT }, () => randomStar(can));
