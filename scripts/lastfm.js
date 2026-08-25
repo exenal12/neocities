474bdaa933e9eef4ff378e01a4fbe028e92bb5fb
Alyx <90263311+exenal12@users.noreply.github.com>
2026-08-24 22:35:02 -0500

    got api info displaying

--- scripts/lastfm.js
+++ scripts/lastfm.js
@@ -1,9 +1,38 @@
 const USERNAME = "drenal1";
 const API_KEY = "c313b179ed4458656126373df436252e";
 const POLL_TIME = 60;
-const URL = "http://ws.audioscrobbler.com/2.0/";
+const URL = "https://ws.audioscrobbler.com/2.0/";
+
+function normalizeTrack(rawTrack) {
+  /* TRACK SHAPE AFTER NORMALIZING:
+  {
+    title: string,
+    imgUrl: string,
+    nowPlaying: boolean,
+    album: string,
+    artist: string,
+    date: string
+  }
+  */
+
+  // grab image of certain size from image array
+  // sizes: small, medium, large, extralarge
+  const imgUrl =
+    rawTrack.image?.find((img) => img.size === "large")?.["#text"] || "";
+  // "Now Playing" guard is redundant, but whatever
+  return {
+    title: rawTrack.name,
+    imgUrl: imgUrl,
+    nowPlaying: rawTrack["@attr"]?.nowplaying === "true",
+    album: rawTrack.album["#text"],
+    artist: rawTrack.artist["#text"],
+    date: rawTrack.date?.["#text"] || "Now Playing",
+  };
+}
 
 async function fetchAPI() {
+  const widget = document.getElementById("lastfm-widget");
+  let text = "";
   const params = new URLSearchParams({
     method: "user.getRecentTracks",
     user: USERNAME,
@@ -14,12 +43,26 @@ async function fetchAPI() {
 
   const requestUrl = `${URL}?${params}`;
 
-  const result = await fetch(requestUrl).then((res) => {
-    if (res.status === 200) {
-      // successful
-    }
-    console.log("results: ", res);
+  const data = await fetch(requestUrl).then(async (res) => {
+    if (res.ok) {
+      return await res.json();
+    } else return;
   });
+
+  return data;
 }
 
-fetchAPI();
+async function renderWidget() {
+  const widget = document.getElementById("lastfm-widget");
+  widget.innerHTML = "";
+  const data = await fetchAPI();
+  const raw = data.recenttracks.track;
+  const tracks = raw.map(normalizeTrack);
+  console.log("tracks: ", tracks);
+
+  // build widget!
+  tracks.forEach((track, index) => {
+    widget.innerHTML += `Track ${index + 1}: ${track.title} by ${track.artist}<br>`;
+  });
+}
+renderWidget();
